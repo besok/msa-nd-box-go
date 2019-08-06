b3c706699b13f438e9fcbb275afa4b0d6ef08210
Besok <zhguchev@gmail.com>
2019-08-06 23:04:39 +0100

    start saga orch

--- saga/orchestrator.go
+++ saga/orchestrator.go
@@ -27,8 +27,8 @@ func (o *Orch) handlerSaga(w http.ResponseWriter, r *http.Request) {
 	var saga message.SagaMessage
 	err := decoder.Decode(&saga)
 	if err != nil {
+		_, _ = w.Write([]byte("can not process"))
 		log.Fatalf(" error %s while parsing json %s \n", err, r.Body)
-		return
 	}
 
 	log.Printf("got a saga message:%+v", saga)
@@ -38,11 +38,20 @@ func (o *Orch) handlerSaga(w http.ResponseWriter, r *http.Request) {
 	idx := 0
 	if len(chapters) == 0 {
 		log.Println("saga does not have chapters, terminate.")
+		_, _ = w.Write([]byte("there are no chapters"))
+		return
 	}
 
 	for ok := true; ok; ok = checkSagaRes(state) {
-		if idx > len(chapters) || idx < 0 {
-			break
+		if idx > len(chapters) {
+			state = message.Finish
+			_, _ = w.Write([]byte(state))
+			return
+		}
+		if idx < 0 {
+			state = message.Abort
+			_, _ = w.Write([]byte(state))
+			return
 		}
 
 		ch := chapters[idx]
@@ -52,36 +61,46 @@ func (o *Orch) handlerSaga(w http.ResponseWriter, r *http.Request) {
 			idx--
 			state = message.Rollback
 		} else {
-			if state == message.Success {
-				url := fmt.Sprintf("http://%s/%s", s, ch.Chapter)
-				buffer := new(bytes.Buffer)
-				_ = json.NewEncoder(buffer).Encode(sagaResultStart(ch))
-				resp, err := http.Post(url, "application/json; charset=utf-8", buffer)
-				if err != nil {
-					log.Println("got error: ", err)
-					idx--
-					state = message.Rollback
-				} else {
-					decoder := json.NewDecoder(resp.Body)
-					var res message.ChapterResult
-					_ := decoder.Decode(&res)
-
-					switch res.State {
-					case message.Success:
-						state = message.Success
-						idx++
-					case message.Rollback:
-						state = message.Rollback
-						idx--
-					default:
-						// finish
-					}
-				}
+			var url string
+			switch state {
+			case message.Success:
+			case message.Start:
+				url = fmt.Sprintf("http://%s/%s", s, ch.Chapter)
+			case message.Rollback:
+				url = fmt.Sprintf("http://%s/%s", s, ch.Rollback)
+			default:
+				_, _ = w.Write([]byte(state))
+				return
 			}
 
-		}
+			buffer := new(bytes.Buffer)
+			_ = json.NewEncoder(buffer).Encode(sagaResultStart(ch))
+			resp, err := http.Post(url, "application/json; charset=utf-8", buffer)
+			if err != nil {
+				log.Println("got error: ", err)
+				idx--
+				state = message.Rollback
+				continue
+			}
 
+			decoder := json.NewDecoder(resp.Body)
+			var res message.ChapterResult
+			_ = decoder.Decode(&res)
+
+			switch res.State {
+			case message.Success:
+				state = message.Success
+				idx++
+			case message.Rollback:
+				state = message.Rollback
+				idx--
+			default:
+				_, _ = w.Write([]byte(state))
+				return
+			}
+		}
 	}
+
 }
 
 func checkSagaRes(state message.ChapterState) bool {
